f9acee730cf85931fdf8e8723301e8b7fa847d3f
wesley <wesleyumsocaminho@gmail.com>
2021-12-26 01:06:55 -0300

    refac: adiciona funcao pra lidar com login, retorna lista com todos os dados, retornar arquivo csv

--- quotes_to_scrape.py
+++ quotes_to_scrape.py
@@ -1,12 +1,25 @@
 
 import requests
-import json
 from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def avalia_login(resposta_login):
+    resposta = {}
+    soup = BeautifulSoup(resposta_login.text, "html.parser")
+    if resposta_login.status_code == 200:
+        resposta['mensagem'] = 'ok'
+        resposta['status_code'] = '200'
+    else:
+        resposta['mensagem'] = 'erro_login'
+        resposta['status_code'] = '400'
+    return resposta
 
 
 def logout(session):
     url_logout = "https://quotes.toscrape.com/logout"
     session.get(url_logout)
+
 
 def login(url, username, password):
     session = requests.Session()
@@ -21,31 +34,32 @@ def login(url, username, password):
         "password": password
     }
     resposta_login = session.post(url=url_login, data=data)
-    if resposta_login.status_code == 200:
-        return resposta_login, session, 'ok'
-    else:
-        return resposta_login, session, 'erro_login'
+    resultado = avalia_login(resposta_login)
+    resultado["session"] = session
+    return resultado
 
 
-def avanca_pagina(soup, sessao):
+def avanca_pagina(soup, sessao,lista_citacoes):
     url = 'https://quotes.toscrape.com'
     try:
         url_prox_pagina = soup.find(
             'li', {"class": "next"}).find('a').get('href')
         prox_pagina = url + url_prox_pagina
-        response_next_pagina = sessao.get(prox_pagina)
-        captura_dados_e_printa(response_next_pagina,sessao)
+        captura_dados_e_printa(sessao=sessao,
+                               url=prox_pagina,
+                               lista_citacoes=lista_citacoes)
     except AttributeError:
         print('Chegou na ultima pagina. ')
 
 
-def captura_dados_e_printa(response, sessao):
+def captura_dados_e_printa(sessao, url, lista_citacoes):
     """recebe um response da pagina 'quotes.toscrape' e printa as informações como:
         'Citacao', 'Autor', 'Tags'
 
     Args:
         response ([Response]): response da pagina de 'quotes.toscrape'
     """
+    response = sessao.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     citacoes = soup.findAll('div', {"class": "quote"})
     for citacao in citacoes:
@@ -60,32 +74,36 @@ def captura_dados_e_printa(response, sessao):
             'autor': author,
             'tags': tags,
         }
-        print(json.dumps(dados,
-                         indent=4,
-                         ensure_ascii=False)
-              )
+        lista_citacoes.append(dados)
 
     avanca_pagina(
-        soup=soup,
-        sessao=sessao
+        soup = soup,
+        sessao = sessao,
+        lista_citacoes=lista_citacoes
     )
+    return lista_citacoes
 
 
 def run():
-    url = 'https://quotes.toscrape.com/'
-    user = 'wesley'
-    password = '12345'
-    s = requests.Session()
-    response_logado, sessao, status_login = login(
-        username=user, password=password, url=url)
-    if status_login == 'ok':
-        response_logado = sessao.get(url)
-        captura_dados_e_printa(response=response_logado,
-                               sessao=sessao)
+    url='https://quotes.toscrape.com/'
+    user='wesley'
+    password='12345'
+    resposta_login=login(
+        username = user, password = password, url = url)
+    sessao=resposta_login['session']
+    if resposta_login['mensagem'] == 'ok':
+        return captura_dados_e_printa(sessao = sessao,
+                               url = url,
+                               lista_citacoes = [])
     else:
         logout(sessao)
         raise Exception('Erro de login. tente novamente')
 
 
 if __name__ == '__main__':
-    run()
+    dados = run()
+    df = pd.DataFrame(dados)
+    print(df)
+    file_name = 'citacoes.csv'
+    df.to_csv(file_name, sep=',')
+    
